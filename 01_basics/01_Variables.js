a87a140a73306698e7822af7da276de1d946da9f
Rakib Mondal <134124152+rakibmondal2507@users.noreply.github.com>
2024-01-09 05:26:31 +0000

    variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,14 @@
+const Name ="Rakib Modnal";
+let email = "rakib@gmail.com";
+var contact = 91000000000;
+city = "Kolkata";
+
+// Name = "rakib"
+
+email = "rakib123@gmail.com";
+contact = 9876543210;
+city = "Barrackpore";
+
+
+console.log(Name);
+console.table([Name , email , contact , city]);
