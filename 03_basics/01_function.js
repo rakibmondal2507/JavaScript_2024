780a7e49c4240396de84415fb73a330ca87b0606
Rakib Mondal <134124152+rakibmondal2507@users.noreply.github.com>
2024-01-19 11:51:04 +0000

    object in function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -24,4 +24,30 @@ function userLogin (username){
     return `${username} just logged in.`;
 }
 // console.log(userLogin("Rakib"));
-console.log(userLogin());
+// console.log(userLogin());
+
+function calculateCartPrice(...num1){
+    return num1;
+}
+// console.log(calculateCartPrice(200,400,600,3000));
+
+// object function 
+user = {
+    name: "Rakib",
+    price: 199
+}
+function handleObject (anyObject){
+    console.log(`Username is ${anyObject.name} and price is ${anyObject.price}`);
+}
+// handleObject(user);
+handleObject({
+    name:"Riyaan",
+    price: 299
+});
+
+const myNewArray = [100,200,400];
+
+function returnSecondValue(getArray){
+    return getArray[0];
+}
+console.log(returnSecondValue(myNewArray));
