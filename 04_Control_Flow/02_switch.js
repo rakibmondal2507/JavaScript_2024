d92e505843969acbe7944f481f040e31d0cd4bc9
Rakib Mondal <134124152+rakibmondal2507@users.noreply.github.com>
2024-01-22 08:52:56 +0000

    switch case

---
+++ 04_Control_Flow/02_switch.js
@@ -0,0 +1,18 @@
+const month =3;
+switch(month){
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    default:
+        console.log("default case match");
+        break;
+}
