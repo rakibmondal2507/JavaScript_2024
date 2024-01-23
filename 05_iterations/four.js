430d52a76400bd8457aeb9d17c447edb33d60196
Rakib Mondal <134124152+rakibmondal2507@users.noreply.github.com>
2024-01-23 09:05:28 +0000

    forIn loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+// forIn loop
+const myObject = {
+    js: "JavaScript",
+    cpp: "C++",
+    rb: "Ruby",
+    swift: "Swift by Apple"
+}
+
+for (const key in myObject) {
+//    console.log(`${key} shortcut for ${myObject[key]}`);
+}
+
+const programming =["java","py","c++","js","rust"];
+
+// for (const key in programming) {
+//    console.log(key);
+// }
+for (const key in programming) {
+//    console.log(programming[key]);
+}
+
+// const map = new Map();
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('FR', "France")
+
+// for (const key in map) {
+//     console.log(key);
+// }
