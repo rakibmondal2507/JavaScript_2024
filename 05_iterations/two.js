ba8ba33ace321998dcdc7bc5ea87f2d65b5b4d85
Rakib Mondal <134124152+rakibmondal2507@users.noreply.github.com>
2024-01-23 06:00:42 +0000

    do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// while loop 
+
+let i = 0;
+while(i<=10){
+    // console.log(`value of i is: ${i}`);
+    i=i+2;
+}
+
+let arr = ["Spiderman","Thor","Ironman"];
+let a=0;
+while(a<arr.length){
+    // console.log(arr[a]);
+    a++;
+}
+
+// do while loop
+let score = 0;
+do {
+    console.log(`value of score is: ${score}`);
+    score++;
+    
+} while (score<=10);
